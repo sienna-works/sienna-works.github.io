688a33a9cc2644d53558515e2794cb26873167a6
jefetienne <etienneayoub@gmail.com>
2026-04-11 17:51:54 -0400

    ..

---
+++ navbar.js
@@ -0,0 +1,46 @@
+function nav() {
+	fetch('navbar.html')
+		.then(response => response.text())
+		.then(data => {
+			document.getElementById('navbar-container').innerHTML = data;
+
+			// Highlight current page (update href matches)
+			const currentPage = window.location.pathname.split('/').pop() || 'index.html';
+			const links = document.querySelectorAll('.nav-link');
+			links.forEach(link => {
+				if (link.getAttribute('href') === currentPage) {
+					link.classList.add('active');
+				}
+			});
+		});
+}
+
+/**
+ * 
+ * @param {string} fileName 
+ */
+function navPage(fileName) {
+	fetch(fileName)
+		.then(response => response.text())
+		.then(data => {
+			document.getElementById('content').innerHTML = data;
+
+			const currentPage = fileName.substring(0, fileName.indexOf('.')).toLowerCase();
+			console.log(currentPage);
+			const links = document.querySelectorAll('.nav-link');
+
+			// Highlight page
+			for (const link of links) {
+				if (link.id === currentPage) {
+					link.classList.add('active');
+				} else {
+					link.classList.remove('active');
+				}
+			}
+		});
+}
+
+function home(){
+	navPage('home.html');
+	fetchPosts();
+}
